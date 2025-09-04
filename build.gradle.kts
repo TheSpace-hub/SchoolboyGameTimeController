3d50627d8a48f6250d0a7f80a3878c1079e07699
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-09-04 18:51:33 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+}
+
+group = "hub.thespace"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+    }
+    maven {
+        url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    }
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    compileOnly("org.spigotmc:spigot-api:1.12.2-R0.1-SNAPSHOT")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/hub/thespace/schoolboygametimecontroller/SchoolboyGameTimeController.java
@@ -0,0 +1,11 @@
+package hub.thespace.schoolboygametimecontroller;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SchoolboyGameTimeController extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello world!");
+    }
+}
